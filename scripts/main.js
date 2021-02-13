7dc16216f3f6a791ccd1d2cf4520c595c00de2ca
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-02-13 12:04:43 +0200

    Add information to the app
    Add information to the app

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+const url = "http://irumvanselme-apis.herokuapp.com/api/messages"
+
 function onHeaderHover (element) {
     element.classList.add("animate__swing", "animate__animated")
     console.log($("#about-text").visible())
@@ -19,4 +21,27 @@ function onAboutOnViewPort (){
         document.getElementById("about-image").classList.remove("animate__swing")
     }, 1000)
 
+}
+
+
+function contactAnselme(event){
+    event.preventDefault();
+
+    // le me get the data 
+
+    let message = {
+        first_name: document.getElementById("first_name").value,
+        last_name: document.getElementById("last_name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    }
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(message)
+    }).then(res => console.log(res))
+    .catch(error => console.log(error))
 }
