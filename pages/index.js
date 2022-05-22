a4cea31abd403e32cf4755959fa47ba07dfc8090
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2022-05-22 18:35:36 +0200

    feat: export v1

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,5 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-    return <div className={styles.container}>hello anselme</div>;
+    return <div className={styles.container}>hello anselme from RCA</div>;
 }
